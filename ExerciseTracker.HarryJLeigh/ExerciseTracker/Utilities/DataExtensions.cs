3c05bffc759109e8956ca94ff64ef41846fe543c
harryL <harryleigh@hotmail.co.uk>
2025-01-05 17:10:01 +0000

    Setup appSettings.json to copy to the output directory.

--- ExerciseTracker.HarryJLeigh/ExerciseTracker/Utilities/DataExtensions.cs
+++ ExerciseTracker.HarryJLeigh/ExerciseTracker/Utilities/DataExtensions.cs
@@ -7,7 +7,7 @@ public static class DataExtensions
     internal static string GetConnectionString()
     {
         var configuration = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName)
+            .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appSettings.json", optional: false, reloadOnChange: true)
             .Build();
         
