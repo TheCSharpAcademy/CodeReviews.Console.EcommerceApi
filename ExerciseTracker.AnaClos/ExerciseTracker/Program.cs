ff7d4995bcf17bedeb27f49b68adbdd35b040213
anaclos <anaclos2@yahoo.com.ar>
2025-02-09 22:06:23 -0300

    Fin dia 5

---
+++ ExerciseTracker.AnaClos/ExerciseTracker/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ExerciseTracker.Controllers;
+using ExerciseTracker.Repositories;
+using ExerciseTracker.UI;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddTransient<IRepository, ExerciseRepository>();
+builder.Services.AddTransient<IController, ExerciseController>();
+builder.Services.AddTransient<UserInput>();
+
+using IHost host = builder.Build();
+
+var serviceProvider = host.Services;
+var exerciseController = serviceProvider.GetRequiredService<IController>();
+exerciseController.Menu();
+host.Run();
---
+++ ExerciseTracker.AnaClos/ExerciseTracker/Services/ExerciseService.cs
@@ -0,0 +1,38 @@
+﻿using ExerciseTracker.Models;
+using ExerciseTracker.Repositories;
+
+namespace ExerciseTracker.Services;
+
+public class ExerciseService
+{
+    public IRepository _exerciseRepository;
+    public ExerciseService(IRepository exerciseRepository)
+    {
+        _exerciseRepository = exerciseRepository;
+    }
+
+    public void Add(Exercise exercise)
+    {
+        _exerciseRepository.Add(exercise);
+    }
+
+    public void Update(Exercise exercise)
+    {
+        _exerciseRepository.Update(exercise);
+    }
+
+    public void Delete(Exercise exercise)
+    {
+        _exerciseRepository.Delete(exercise);
+    }
+
+    Exercise GetById(int id)
+    {
+        return _exerciseRepository.GetById(id);
+    }
+
+    IEnumerable<Exercise> GetAll()
+    {
+        return _exerciseRepository.GetAll();
+    }
+}
