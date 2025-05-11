d8fd9ea3248a7eb13ddca7358d1afe3508482b84
Daniel <Daniel@DESKTOP-EICR6RV>
2025-05-11 17:45:35 +0200

    initial project stucture created

---
+++ ExerciseTracker.Brozda/ExerciseTracker.Brozda/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExerciseTracker.Brozda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
