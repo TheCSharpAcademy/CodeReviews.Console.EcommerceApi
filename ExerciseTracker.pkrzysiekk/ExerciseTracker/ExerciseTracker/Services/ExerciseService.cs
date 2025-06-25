39905a9594ef88e975c3aa59e62a0470ec700b17
pkrzysiek <pkrzysiek24@gmail.com>
2025-06-25 17:36:16 +0200

    Add service layer

---
+++ ExerciseTracker.pkrzysiekk/ExerciseTracker/ExerciseTracker/Services/ExerciseService.cs
@@ -0,0 +1,34 @@
+using ExerciseTracker.Models;
+using ExerciseTracker.Repository;
+
+namespace ExerciseTracker.Services;
+
+public class ExerciseService : IExerciseService
+{
+    private readonly IRepository<Exercise> _repository;
+    
+    public ExerciseService(IRepository<Exercise> repository) 
+    {
+       _repository = repository; 
+    }
+
+    public async Task AddExercise(Exercise exercise)
+    {
+        await _repository.Insert(exercise);
+    }
+
+    public async Task DeleteExercise(Exercise exercise)
+    {
+        await _repository.Delete(exercise);
+    }
+
+    public async Task EditExercise(Exercise exercise)
+    {
+        await _repository.Update(exercise);
+    }
+
+    public async Task<IEnumerable<Exercise>> GetAllExercises()
+    {
+        return await _repository.GetAll();
+    }
+}
---
+++ ExerciseTracker.pkrzysiekk/ExerciseTracker/ExerciseTracker/Services/IExerciseService.cs
@@ -0,0 +1,11 @@
+using ExerciseTracker.Models;
+
+namespace ExerciseTracker.Services;
+
+public interface IExerciseService
+{
+    public Task AddExercise(Exercise exercise);
+    public Task DeleteExercise(Exercise exercise);
+    public Task EditExercise(Exercise exercise);
+    public Task<IEnumerable<Exercise>> GetAllExercises();
+}
