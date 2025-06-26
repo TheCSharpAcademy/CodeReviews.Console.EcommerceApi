35990ab45ea3861f2f97b77ef088fef31687605d
pkrzysiek <pkrzysiek24@gmail.com>
2025-06-26 18:58:46 +0200

    Add table helper

---
+++ ExerciseTracker.pkrzysiekk/ExerciseTracker/ExerciseTracker/Menu/MenuHelpers/TableHelper.cs
@@ -0,0 +1,26 @@
+using ExerciseTracker.Models;
+using Spectre.Console;
+
+namespace ExerciseTracker.Menu.MenuHelpers;
+
+public static class TableHelper
+{
+    public static void Show(IEnumerable<Exercise> exercises)
+    {
+        var table = new Table();
+        
+        table.AddColumn("Description");
+        table.AddColumn("Start Date");
+        table.AddColumn("End Date");
+        foreach (var exercise in exercises)
+        {
+            table.AddRow(exercise.Description,
+                exercise.DateStart.ToString("yyyy/MM/dd HH:mm:ss UTC")
+                ,exercise.DateEnd.ToString("yyyy/MM/dd HH:mm:ss UTC"));
+     
+            
+        } 
+        AnsiConsole.Write(table);
+    }
+    
+}
