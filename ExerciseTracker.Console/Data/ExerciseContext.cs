a8e4e64bcbbff3634f43e52f20998d7c30904990
niasua <nsuarez2410@gmail.com>
2025-08-04 20:42:12 -0300

    ExerciseContext configured

---
+++ ExerciseTracker.Console/Data/ExerciseContext.cs
@@ -0,0 +1,12 @@
+﻿using ExerciseTracker.Console.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExerciseTracker.Console.Data;
+
+public class ExerciseContext : DbContext
+{
+    public DbSet<Exercise> Exercises { get; set; }
+    public ExerciseContext(DbContextOptions<ExerciseContext> options) : base(options)
+    {
+    }
+}
--- ExerciseTracker.Console/Program.cs
+++ ExerciseTracker.Console/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ExerciseTracker.Console.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.json", optional: false)
+    .Build();
+
+var connectionString = config.GetConnectionString("DefaultConnection");
+
+var optionsBuilder = new DbContextOptionsBuilder<ExerciseContext>();
+optionsBuilder.UseSqlServer(connectionString);
+
+using var context = new ExerciseContext(optionsBuilder.Options);
