9b3df9cbc603df1b264ebb77dd890f5c8794af28
niasua <nsuarez2410@gmail.com>
2025-08-05 17:29:57 -0300

    adds display class. adds show exercises method

---
+++ ExerciseTracker.Console/UI/Display.cs
@@ -0,0 +1,31 @@
+﻿using ExerciseTracker.Niasua.Models;
+using Spectre.Console;
+
+namespace ExerciseTracker.Niasua.UI;
+
+public static class Display
+{
+    public static void ShowExercises(List<Exercise> exercises)
+    {
+        var table = new Table();
+        table.Border(TableBorder.Rounded);
+        table.AddColumn("[red]Id[/]");
+        table.AddColumn("[yellow]Date Start[/]");
+        table.AddColumn("[blue]Date End[/]");
+        table.AddColumn("[green]Duration[/]");
+        table.AddColumn("[cyan]Comments[/]");
+
+        foreach (var exercise in exercises)
+        {
+            table.AddRow(
+                exercise.Id.ToString(),
+                exercise.DateStart.ToString("g"),
+                exercise.DateEnd.ToString("g"),
+                exercise.Duration.ToString("g"),
+                string.IsNullOrWhiteSpace(exercise.Comments) ? "[grey]None[/]" : exercise.Comments
+                );
+        }
+
+        AnsiConsole.Write(table);
+    }
+}
