304f7f549e181ad4ddf25e4fff0a1500bb5fc4d4
niasua <nsuarez2410@gmail.com>
2025-08-04 21:35:40 -0300

    adds exercise service basic structure

---
+++ ExerciseTracker.Console/Services/ExerciseService.cs
@@ -0,0 +1,39 @@
+﻿using ExerciseTracker.Console.Models;
+using ExerciseTracker.Console.Repositories;
+
+namespace ExerciseTracker.Console.Services;
+
+public class ExerciseService
+{
+    private readonly ExerciseRepository _repository;
+
+    public ExerciseService(ExerciseRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task CreateExerciseAsync(Exercise exercise)
+    {
+        await _repository.AddAsync(exercise);
+    }
+
+    public async Task DeleteExerciseAsync(int id)
+    {
+        await _repository.DeleteAsync(id);
+    }
+
+    public async Task<List<Exercise>> GetAllExercisesAsync()
+    {
+        return await _repository.GetAllAsync();
+    }
+
+    public async Task<Exercise?> GetExercisesByIdAsync(int id)
+    {
+        return await _repository.GetByIdAsync(id);
+    }
+
+    public async Task UpdateExercisesAsync(Exercise exercise)
+    {
+        await _repository.UpdateAsync(exercise);
+    }
+}
