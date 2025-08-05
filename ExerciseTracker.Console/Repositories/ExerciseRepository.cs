31e699966ed6486859cdff442327f16c7e80f5a0
niasua <nsuarez2410@gmail.com>
2025-08-04 21:20:50 -0300

    adds exercise repo and its interface

---
+++ ExerciseTracker.Console/Repositories/ExerciseRepository.cs
@@ -0,0 +1,47 @@
+﻿using ExerciseTracker.Console.Data;
+using ExerciseTracker.Console.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExerciseTracker.Console.Repositories;
+
+public class ExerciseRepository : IExerciseRepository
+{
+    private readonly ExerciseContext _context;
+
+    public ExerciseRepository(ExerciseContext context)
+    {
+        _context = context;
+    }
+
+    public async Task AddAsync(Exercise exercise)
+    {
+        await _context.Exercises.AddAsync(exercise);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var exercise = await GetByIdAsync(id);
+        if (exercise != null)
+        {
+            _context.Exercises.Remove(exercise);
+            await _context.SaveChangesAsync();
+        }
+    }
+
+    public async Task<List<Exercise>> GetAllAsync()
+    {
+        return await _context.Exercises.ToListAsync();
+    }
+
+    public async Task<Exercise?> GetByIdAsync(int id)
+    {
+        return await _context.Exercises.FindAsync(id);
+    }
+
+    public async Task UpdateAsync(Exercise exercise)
+    {
+        _context.Exercises.Update(exercise);
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ ExerciseTracker.Console/Repositories/IExerciseRepository.cs
@@ -0,0 +1,12 @@
+﻿using ExerciseTracker.Console.Models;
+
+namespace ExerciseTracker.Console.Repositories;
+
+public interface IExerciseRepository
+{
+    Task<List<Exercise>> GetAllAsync();
+    Task<Exercise?> GetByIdAsync(int id);
+    Task AddAsync(Exercise exercise);
+    Task UpdateAsync(Exercise exercise);
+    Task DeleteAsync(int id);
+}
