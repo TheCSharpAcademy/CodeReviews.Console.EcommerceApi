b6832301269b9c355898d7461861c6a468adbef5
niasua <nsuarez2410@gmail.com>
2025-08-05 17:17:12 -0300

    adds delete exercise in controller

--- ExerciseTracker.Console/Controllers/ExerciseController.cs
+++ ExerciseTracker.Console/Controllers/ExerciseController.cs
@@ -1,5 +1,7 @@
 ﻿using ExerciseTracker.Niasua.Services;
 using ExerciseTracker.Niasua.UserInput;
+using ExerciseTracker.Niasua.Validators;
+using Spectre.Console;
 
 namespace ExerciseTracker.Niasua.Controllers;
 
@@ -12,15 +14,34 @@ public class ExerciseController
         _service = service;
     }
     
-    public async Task AddExercise()
+    public async Task AddExerciseAsync()
     {
         var exercise = UserInputHandler.GetExerciseInput();
 
         var success = await _service.CreateExerciseAsync(exercise);
 
         if (success)
-            Console.WriteLine("Exercise successfully added.");
+            AnsiConsole.MarkupLine("[green]Exercise successfully added.[/]");
         else
-            Console.WriteLine("Failed to add exercise.");
+            AnsiConsole.MarkupLine("[red]Failed to add exercise.[/]");
+    }
+
+    public async Task DeleteExerciseAsync()
+    {
+        var idInput = AnsiConsole.Ask<string>("Type Exercise's ID:");
+        if (!int.TryParse(idInput, out int id))
+        {
+            AnsiConsole.MarkupLine("[red]Invalid ID.[/]");
+            return;
+        }
+
+        if (!await ExerciseValidator.ExerciseExistsById(id))
+        {
+            AnsiConsole.MarkupLine("[red]Failed to delete exercise.[/]");
+            return;
+        }
+
+        await _service.DeleteExerciseAsync(id);
+        AnsiConsole.MarkupLine("[green]Exercise successfully deleted.[/]");
     }
 }
