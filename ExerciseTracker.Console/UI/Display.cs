49ea65015a064209fa6bd76a00451c7924313d00
niasua <nsuarez2410@gmail.com>
2025-08-05 17:44:39 -0300

    adds a show exercise method in display

--- ExerciseTracker.Console/UI/Display.cs
+++ ExerciseTracker.Console/UI/Display.cs
@@ -28,4 +28,26 @@ public static class Display
 
         AnsiConsole.Write(table);
     }
+
+    public static void ShowExercise(Exercise exercise)
+    {
+        var table = new Table();
+        table.Border(TableBorder.Rounded);
+        table.AddColumn("[red]Id[/]");
+        table.AddColumn("[yellow]Date Start[/]");
+        table.AddColumn("[blue]Date End[/]");
+        table.AddColumn("[green]Duration[/]");
+        table.AddColumn("[cyan]Comments[/]");
+
+        
+        table.AddRow(
+            exercise.Id.ToString(),
+            exercise.DateStart.ToString("g"),
+            exercise.DateEnd.ToString("g"),
+            exercise.Duration.ToString("g"),
+            string.IsNullOrWhiteSpace(exercise.Comments) ? "[grey]None[/]" : exercise.Comments
+            );
+        
+        AnsiConsole.Write(table);
+    }
 }
