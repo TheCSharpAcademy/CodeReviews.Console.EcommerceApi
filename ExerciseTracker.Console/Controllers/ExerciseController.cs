d9e3e1cb06ce162675db60f77b5e195e55bec8bb
niasua <nsuarez2410@gmail.com>
2025-08-04 22:03:21 -0300

    adds exercise controller (add exercise method only)

---
+++ ExerciseTracker.Console/Controllers/ExerciseController.cs
@@ -0,0 +1,26 @@
+﻿using ExerciseTracker.Niasua.Services;
+using ExerciseTracker.Niasua.UserInput;
+
+namespace ExerciseTracker.Niasua.Controllers;
+
+public class ExerciseController
+{
+    private readonly ExerciseService _service;
+
+    public ExerciseController(ExerciseService service)
+    {
+        _service = service;
+    }
+    
+    public async Task AddExercise()
+    {
+        var exercise = UserInputHandler.GetExerciseInput();
+
+        var success = await _service.CreateExerciseAsync(exercise);
+
+        if (success)
+            Console.WriteLine("Exercise successfully added.");
+        else
+            Console.WriteLine("Failed to add exercise.");
+    }
+}
