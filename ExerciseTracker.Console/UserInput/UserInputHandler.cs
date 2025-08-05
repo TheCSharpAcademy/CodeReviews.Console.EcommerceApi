b69eee10632587f891a0c62ff131594eaa64c4d0
niasua <nsuarez2410@gmail.com>
2025-08-04 22:02:18 -0300

    adds user input handler

---
+++ ExerciseTracker.Console/UserInput/UserInputHandler.cs
@@ -0,0 +1,38 @@
+﻿using ExerciseTracker.Niasua.Models;
+
+namespace ExerciseTracker.Niasua.UserInput;
+
+public static class UserInputHandler
+{
+    public static Exercise GetExerciseInput()
+    {
+        Console.WriteLine("Enter exercise start date and time (yyyy-mm-dd hh:mm)");
+        DateTime start = ReadDateTime();
+
+        Console.WriteLine("Enter exercise start date and time (yyyy-mm-dd hh:mm)");
+        DateTime end = ReadDateTime();
+
+        Console.WriteLine("Enter any comments: ");
+        string? comments = Console.ReadLine();
+
+        return new Exercise
+        {
+            DateStart = start,
+            DateEnd = end,
+            Comments = comments
+        };
+    }
+
+    private static DateTime ReadDateTime()
+    {
+        while (true)
+        {
+            var input = Console.ReadLine();
+
+            if (DateTime.TryParse(input, out var result))
+                return result;
+
+            Console.WriteLine("Invalid format. Please use format like: yyyy-mm-dd hh:mm");
+        }
+    }
+}
